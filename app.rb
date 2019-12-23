8f3027baf52067fb2804d5d5881bc0c82d60e2e5
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-23 22:30:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
+    "Hello, World!"
+  end
+  get '/' do
     "Hell, World!"
   end
-  
+  get 'hometwon' do
+    "Hell, World!"
+  end
+  get 'favorite-song' do
+    "Hell, World!"
+  end
 end
